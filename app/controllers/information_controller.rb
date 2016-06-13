1d04796e1d29ffaf6ef88d28de59019581471a78
Artem Ankudovich <artemankudovich@gmail.com>
2016-06-13 15:17:44 +0100

    added most popular pages list could be beneficial for reporting

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -2,4 +2,7 @@ class InformationController < ApplicationController
 	def home
 
     end
+    def most_viewed_pages
+    	@pages = Page.all.sort_by{|post| post.impressions.count}.last 10
+    end
 end
